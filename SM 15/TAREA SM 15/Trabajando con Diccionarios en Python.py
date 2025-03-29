c28061c1b93a5848d3a28cf47e23fe2d7ae95600
EDFANOBA <ef.noriegab@uea.edu.ec>
2025-03-28 22:54:24 -0500

    Update Trabajando con Diccionarios en Python.py

--- SM 15/TAREA SM 15/Trabajando con Diccionarios en Python.py
+++ SM 15/TAREA SM 15/Trabajando con Diccionarios en Python.py
@@ -15,17 +15,17 @@ informacion_personal["ciudad"] = "San Miguel de Bolivar"
 # Agrega una nueva clave-valor para representar la "profesión"
 informacion_personal["profesión"] = "Estudiante de Tecnologias de la Informacion"
 
-# 3. Verificar Existencia de Claves
+# PASO 3. Verificar Existencia de Claves
 # Verifica si la clave "teléfono" existe en el diccionario
 if "teléfono" not in informacion_personal:
     # Si no existe, agrégala con un número de teléfono ficticio
     informacion_personal["teléfono"] = "0990937519"
 
-# 4. Eliminar una Clave
+# PASO 4. Eliminar una Clave
 # Elimina la clave "edad" del diccionario
 del informacion_personal["edad"]
 
-# 5. Imprimir el Diccionario Final
+# PASO 5. Imprimir el Diccionario Final
 # Imprime el diccionario resultante después de realizar todas las operaciones
 print("Información Personal Final:")
 print(informacion_personal)
